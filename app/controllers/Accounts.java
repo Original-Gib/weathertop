1dfba2be1b680391f5b85f75df93ed336ca4f8f4
Adam Gibson <adamhenrygibson@gmail.com>
2022-05-17 22:05:12 +0100

    added login/logout options

--- app/controllers/Accounts.java
+++ app/controllers/Accounts.java
@@ -1,2 +1,59 @@
-package controllers;public class Accounts {
+package controllers;
+
+import jdk.internal.net.http.common.Log;
+import models.Member;
+import play.Logger;
+import play.mvc.Controller;
+
+public class Accounts extends Controller {
+
+    public static void signup(){
+
+        render("signup.html");
+    }
+
+    public static void login(){
+
+        render("login.html");
+    }
+
+    public static void register(String firstname, String lastname, String email, String password) {
+
+        Logger.info("Registering new member: " + email);
+        Member member = new Member(firstname, lastname, email, password);
+        member.save();
+        redirect("/");
+    }
+
+    public  static void authenticate(String email, String password){
+
+        Logger.info("Attempting to authenticate with: " + email + " + " + password);
+
+        Member member = Member.findByEmail(email);
+        if ((member != null) && (member.checkPassword(password) == true)) {
+            Logger.info("Authenication Successful");
+            session.put("logged_in_Memberid", member.id);
+            redirect("/dashboard");
+        } else {
+            Logger.info("Authentication Failed");
+            redirect("/login");
+        }
+    }
+
+    public static void logout() {
+        session.clear();
+        redirect ("/");
+    }
+
+    public static Member getLoggedInMember()
+    {
+        Member member = null;
+        if (session.contains("logged_in_Memberid")) {
+            String memberId = session.get("logged_in_Memberid");
+            member = Member.findById(Long.parseLong(memberId));
+        } else {
+            login();
+        }
+        return member;
+    }
 }
--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Member;
 import models.Station;
 import models.Reading;
 import play.Logger;
@@ -12,17 +13,20 @@ import java.util.List;
 public class Dashboard extends Controller
 {
   public static void index() {
+
     Logger.info("Rendering Dashboard");
-
-    List<Station> stations = Station.findAll();
-
-    render ("dashboard.html", stations);
+    Member member = Accounts.getLoggedInMember();
+    List<Station> stations = member.stations;
+    render ("dashboard.html", member, stations);
   }
 
   public static void addStation(String name){
-    Station station = new Station(name);
+
     Logger.info("Adding new station: " + name);
-    station.save();
+    Member member = Accounts.getLoggedInMember();
+    Station station = new Station(name);
+    member.stations.add(station);
+    member.save();
     redirect("/dashboard");
   }
 
