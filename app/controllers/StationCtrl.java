792ead241f9a50ff2aa64bda46d6337718b13289
Adam Gibson <adamhenrygibson@gmail.com>
2022-05-18 20:55:38 +0100

    added lat and long options for stations - reformatted the last reading stats on the station page

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.List;
 
+import models.Member;
 import models.Station;
 import models.Reading;
 import play.Logger;
@@ -10,32 +11,35 @@ import utils.StationAnalytics;
 
 public class StationCtrl extends Controller {
 
-    public static void index(long id){
+  public static void index(long id) {
 
-        Station station = Station.findById(id);
+    Station station = Station.findById(id);
 
-        if(station.readings.size() !=0) {
-            Reading lastReading = StationAnalytics.getLastReading(station.readings);
-            String weatherCondition = StationAnalytics.weatherCondition(lastReading.code);
-            double fahrenheit = StationAnalytics.celciusToFahrenheit(lastReading.temperature);
-            int beaufort = StationAnalytics.beaufortConversion(lastReading.windSpeed);
-            String windDirection = StationAnalytics.windDirectionCompass(lastReading.windDirection);
-            double windChill = StationAnalytics.calculateWindChill(lastReading.temperature, lastReading.windSpeed);
+    if (station.readings.size() != 0) {
+      Reading lastReading = StationAnalytics.getLastReading(station.readings);
+      String weatherCondition = StationAnalytics.weatherCondition(lastReading.code);
+      double fahrenheit = StationAnalytics.celciusToFahrenheit(lastReading.temperature);
+      int beaufort = StationAnalytics.beaufortConversion(lastReading.windSpeed);
+      String windDirection = StationAnalytics.windDirectionCompass(lastReading.windDirection);
+      double windChill = StationAnalytics.calculateWindChill(lastReading.temperature, lastReading.windSpeed);
+      double latitude = station.getLatitude();
+      double longitude = station.getLongitude();
+      Member member = Accounts.getLoggedInMember();
 
 
-            Logger.info("Rendering station: " + id);
+      Logger.info("Rendering station: " + id);
 
-            render("station.html", station, lastReading, weatherCondition, fahrenheit, beaufort, windDirection, windChill);
-        } else {
-            render("station.html", station);
-        }
+      render("station.html", station, lastReading, weatherCondition, fahrenheit, beaufort, windDirection, windChill, latitude, longitude, member);
+    } else {
+      render("station.html", station);
     }
+  }
 
-    public static void addReading(Long id, int code, double temperature, double windSpeed, double windDirection, int pressure){
-        Reading reading = new Reading(code, temperature, windSpeed, windDirection, pressure);
-        Station station = Station.findById(id);
-        station.readings.add(reading);
-        station.save();
-        redirect("/stations/" + id);
-    }
+  public static void addReading(Long id, int code, double temperature, double windSpeed, double windDirection, int pressure) {
+    Reading reading = new Reading(code, temperature, windSpeed, windDirection, pressure);
+    Station station = Station.findById(id);
+    station.readings.add(reading);
+    station.save();
+    redirect("/stations/" + id);
+  }
 }
--- app/models/Station.java
+++ app/models/Station.java
@@ -13,12 +13,47 @@ import play.jobs.On;
 @Entity
 public class Station extends Model{
     public String name;
+    public double latitude;
+    public double longitude;
 
     @OneToMany(cascade = CascadeType.ALL)
     public List<Reading> readings = new ArrayList<Reading>();
 
-    public Station(String name) {
+    public Station(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public List<Reading> getReadings() {
+        return readings;
+    }
+
+    public void setReadings(List<Reading> readings) {
+        this.readings = readings;
+    }
 }
