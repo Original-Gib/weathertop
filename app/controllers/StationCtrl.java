de97d22fc2842af0e009bdc502dcc6da755242bf
Adam Gibson <adamhenrygibson@gmail.com>
2022-05-19 21:32:18 +0100

    added station summary on dashboard page

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -15,25 +15,12 @@ public class StationCtrl extends Controller {
 
     Station station = Station.findById(id);
 
-    if (station.readings.size() != 0) {
-      Reading lastReading = StationAnalytics.getLastReading(station.readings);
-      String weatherCondition = StationAnalytics.weatherCondition(lastReading.code);
-      double fahrenheit = StationAnalytics.celciusToFahrenheit(lastReading.temperature);
-      int beaufort = StationAnalytics.beaufortConversion(lastReading.windSpeed);
-      String windDirection = StationAnalytics.windDirectionCompass(lastReading.windDirection);
-      double windChill = StationAnalytics.calculateWindChill(lastReading.temperature, lastReading.windSpeed);
-      double latitude = station.getLatitude();
-      double longitude = station.getLongitude();
-      Member member = Accounts.getLoggedInMember();
-
 
       Logger.info("Rendering station: " + id);
 
-      render("station.html", station, lastReading, weatherCondition, fahrenheit, beaufort, windDirection, windChill, latitude, longitude, member);
-    } else {
       render("station.html", station);
-    }
   }
+
 
   public static void addReading(Long id, int code, double temperature, double windSpeed, double windDirection, int pressure) {
     Reading reading = new Reading(code, temperature, windSpeed, windDirection, pressure);
--- app/models/Station.java
+++ app/models/Station.java
@@ -56,4 +56,182 @@ public class Station extends Model{
     public void setReadings(List<Reading> readings) {
         this.readings = readings;
     }
+
+    public String latestWeatherCondition(){
+        if (readings.size() != 0) {
+            int index = readings.size() - 1;
+            Reading reading = readings.get(index);
+            String weatherCondition = null;
+            switch (reading.code) {
+                case 100:
+                    weatherCondition = "Clear";
+                    break;
+                case 200:
+                    weatherCondition = "Partial Clouds";
+                    break;
+                case 300:
+                    weatherCondition = "Cloudy";
+                    break;
+                case 400:
+                    weatherCondition = "Light Showers";
+                    break;
+                case 500:
+                    weatherCondition = "Heavy Showers";
+                    break;
+                case 600:
+                    weatherCondition = "Rain";
+                    break;
+                case 700:
+                    weatherCondition = "Snow";
+                    break;
+                case 800:
+                    weatherCondition = "Thunder";
+                    break;
+                default:
+                    weatherCondition = "Invalid Reading";
+                    break;
+            }
+            return weatherCondition;
+        } else {
+            return null;
+        }
+    }
+
+    public double celciusToFahrenheit (){
+        if (readings.size() != 0) {
+            int index = readings.size() - 1;
+            Reading reading = readings.get(index);
+            double fahrenheit = reading.temperature * 9 / 5 + 32;
+            return fahrenheit;
+        } else {
+            double fallBack = 0.0;
+            return fallBack;
+        }
+    }
+
+    public double latestTemperature(){
+        if (readings.size() != 0) {
+            int index = readings.size() - 1;
+            Reading reading = readings.get(index);
+            double temperature = reading.temperature;
+            return temperature;
+        } else {
+            double fallBack = 0.0;
+            return fallBack;
+        }
+    }
+
+    public int beaufortConversion () {
+        if (readings.size() != 0) {
+            int index = readings.size() - 1;
+            Reading reading = readings.get(index);
+            double windSpeed = reading.windSpeed;
+            int beaufort;
+            if (windSpeed < 1) {
+                beaufort = 0;
+            } else if (windSpeed >= 1 && windSpeed < 6) {
+                beaufort = 1;
+            } else if (windSpeed >= 6 && windSpeed < 12) {
+                beaufort = 2;
+            } else if (windSpeed >= 12 && windSpeed < 20) {
+                beaufort = 3;
+            } else if (windSpeed >= 20 && windSpeed < 29) {
+                beaufort = 4;
+            } else if (windSpeed >= 29 && windSpeed < 39) {
+                beaufort = 5;
+            } else if (windSpeed >= 39 && windSpeed < 50) {
+                beaufort = 6;
+            } else if (windSpeed >= 50 && windSpeed < 62) {
+                beaufort = 7;
+            } else if (windSpeed >= 62 && windSpeed < 75) {
+                beaufort = 8;
+            } else if (windSpeed >= 75 && windSpeed < 89) {
+                beaufort = 9;
+            } else if (windSpeed >= 89 && windSpeed < 103) {
+                beaufort = 10;
+            } else if (windSpeed >= 103 && windSpeed < 118) {
+                beaufort = 11;
+            } else {
+                beaufort = 0;
+            }
+            return beaufort;
+        } else {
+            int fallBack = 0;
+            return fallBack;
+        }
+    }
+
+    public String windDirectionCompass (){
+        if (readings.size() != 0) {
+            int index = readings.size() - 1;
+            Reading reading = readings.get(index);
+            double windDirection = reading.windDirection;
+            String windDirectionCompass = null;
+            if ((windDirection >= 348.75) && (windDirection <= 360) || (windDirection >= 0) && (windDirection < 11.25)) {
+                windDirectionCompass = "North";
+            } else if (windDirection >= 11.25 && windDirection < 33.75) {
+                windDirectionCompass = "North North-East";
+            } else if (windDirection >= 33.75 && windDirection < 56.25) {
+                windDirectionCompass = "North-East";
+            } else if (windDirection >= 56.25 && windDirection < 78.75) {
+                windDirectionCompass = "East North-East";
+            } else if (windDirection >= 78.75 && windDirection < 101.25) {
+                windDirectionCompass = "East";
+            } else if (windDirection >= 101.25 && windDirection < 123.75) {
+                windDirectionCompass = "East South-East";
+            } else if (windDirection >= 123.75 && windDirection < 146.25) {
+                windDirectionCompass = "South-East";
+            } else if (windDirection >= 146.25 && windDirection < 168.75) {
+                windDirectionCompass = "South South-East";
+            } else if (windDirection >= 168.75 && windDirection < 191.25) {
+                windDirectionCompass = "South";
+            } else if (windDirection >= 191.25 && windDirection < 213.75) {
+                windDirectionCompass = "South South-West";
+            } else if (windDirection >= 213.75 && windDirection < 236.25) {
+                windDirectionCompass = "South-West";
+            } else if (windDirection >= 236.25 && windDirection < 258.75) {
+                windDirectionCompass = "West South-West";
+            } else if (windDirection >= 258.75 && windDirection < 281.25) {
+                windDirectionCompass = "West";
+            } else if (windDirection >= 281.25 && windDirection < 303.75) {
+                windDirectionCompass = "West North-West";
+            } else if (windDirection >= 303.75 && windDirection < 326.25) {
+                windDirectionCompass = "North-West";
+            } else if (windDirection >= 326.25 && windDirection < 348.75) {
+                windDirectionCompass = "North North-West";
+            } else {
+                windDirectionCompass = "Error";
+            }
+            return windDirectionCompass;
+        } else {
+            return null;
+        }
+    }
+
+    public double calculateWindChill(){
+        if (readings.size() != 0) {
+            int index = readings.size() - 1;
+            Reading reading = readings.get(index);
+            double windChill = 13.12 + 0.6215 * reading.temperature - 11.37 * Math.pow(reading.windSpeed, 0.16) + 0.3965 * reading.temperature * Math.pow(reading.windSpeed, 0.16);
+            return windChill;
+        } else {
+            double fallBack = 0.0;
+            return fallBack;
+        }
+    }
+
+    public int lastReadingPressure() {
+        if (readings.size() != 0) {
+            int index = readings.size() - 1;
+            Reading reading = readings.get(index);
+            int pressure = reading.pressure;
+            return pressure;
+        } else {
+            int fallBack = 0;
+            return fallBack;
+        }
+    }
+
+
+
 }
